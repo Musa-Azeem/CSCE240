870491d52a7e939f6145c4faff991f8bcfe5fa45
MMAzeem3 <musasa12399@gmail.com>
2021-12-02 23:31:16 -0500

    main

--- Final/main.cpp
+++ Final/main.cpp
@@ -6,5 +6,9 @@ using namespace std;
 
 
 int main(int argc, char **argv){
-    
+    myData dataA(10, 3, 10);
+    myData dataB(dataA);
+    dataA.print();
+    cout << "_______________________________________" << endl;
+    dataB.print();
 }
