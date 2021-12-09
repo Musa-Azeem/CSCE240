90c5c2ef3e08552868eff24708a8d7c80bcfb29e
MMAzeem3 <musasa12399@gmail.com>
2021-12-08 23:05:37 -0500

    main test

--- Final/submission/main.cpp
+++ Final/submission/main.cpp
@@ -38,24 +38,34 @@ int main(int argc, char** argv)
         exit(1);
     }
 
-    myData data1;
-    myData data2(obs, dim, 0);
+    //TESTING
 
-    in_file >> data2;
-    data2.Summary();
-    fitness = data2.kMeansClustering(2, 100, 0.001);
+    myData data1;               //3) Default
+    myData data2(obs, dim, 0);  //4)    Alt
+    //5
+    myData data3(data2);        //6) copy
+    data1 = data2;              //7) assignment
+    cout << "data1==data2" << (data1==data2) << endl;   //8) equality
+    cout << "data1!=data2" << (data1!=data2) << endl;   //9) inequality
+    myData data4 = data1+data2; //10) addition
+    cout << data4;
+    //11    no setters because they would not be appropriate
+
+    in_file >> data2;           //12) >>
+    data2.Summary();            //14) Summary
+    fitness = data2.kMeansClustering(2, 100, 0.001);    //15) kMeansClustering
     cout << "K-Means Cluster with 2 clusters produced a fitness of " << fitness;
     cout << endl;
-    //kMeansClustering.ClusterSummary();
-    data2.ClusterSummary();
+    data2.ClusterSummary();     //16) ClusterSummary
 
-    myData data3(data2);
-    out_file << data2;
+    //17 []
+    data2[0] = 5;
+    cout << data2[0] << endl;
 
-    data1 = data2;
+    out_file << data2;      //13) <<
+
 
     in_file.close();
     out_file.close();
-    
     return(0);
 }
