7bf30032c6be560cfc523ef7c805a75ec51a3c38
MMAzeem3 <musasa12399@gmail.com>
2021-12-09 17:19:28 -0500

    comment kMeans deleted redundant line

--- Final/submission/kMeans.h
+++ Final/submission/kMeans.h
@@ -1,3 +1,25 @@
+/*
+Written by Musa Azeem
+Completed:  
+This program defines the kMeans object
+This object contains n number of clusters (Clust objects), and can recieve a dataset to complete kMeans Clustering on it
+Constructor Input Options are:
+    none (default)
+    number of coordinates, number of clusters, and a range to initalize clusters in
+    another instance of kMeans to copy
+Other Input:
+    Assignment operator to assign object to another kMeans object
+    kMeansClustering function:  
+        an array of Points (with same dimensions as cluster centroids)
+        size of the array
+        maximum number of iterations to complete
+        tolerance of kMeans Clustering
+Functions that Output data:
+    stream insertion operator prints the clusters' centroids to ostream
+    kMeansClustering modifies memberships of Points given to it and returns the fitness of kMeans Clustering
+    ClusterSummary prints the cluster data to stdout
+
+*/
 #ifndef KMEANS_H
 #define KMEANS_H
 #include "Point.h"
